8b0a28a21a64889b9a3953157d0f43d15bc37484
Jacob Sobolev <yakovsobolev1@gmail.com>
2016-05-25 17:27:53 +0300

    finished lab 10 ex 1

--- algorithms/lab10/main.cpp
+++ algorithms/lab10/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void lab10ex1();
 vector<int> coinReturn(vector<int> &d, int k);
-void printCoin(vector<int> &c);
+void printCoin(vector<int> &c,vector<int> &d,int i);
 
 void lab10ex2();
 
@@ -20,6 +20,7 @@ void lab10ex1()
 	//vector<int> d = {1,4,6};
 	vector<int> d = { 1,5,10,50,100 };
 	vector<int> c;
+	int index = 7;
 	int k = 9;
 	c = coinReturn(d, k);
 	for (int i = 0; i < c.size(); i++)
@@ -34,6 +35,10 @@ void lab10ex1()
 
 	}
 	cout << endl;
+	cout << "The path is:" << endl;
+	printCoin(c,d, index);
+	cout << endl;
+
 }
 
 vector<int> coinReturn(vector<int>& d, int k)
@@ -68,3 +73,75 @@ vector<int> coinReturn(vector<int>& d, int k)
 
 	return returnVec;
 }
+
+void printCoin(vector<int>& c, vector<int>& d, int i)
+{
+	vector<int> sortV;
+	int index = c[i];
+	
+	
+	if (i < d[0]) {
+		if (i == index) {
+			cout << i << " = " << index << endl;
+		}
+		else {
+			cout << "can't add to this sum with the current coins value" << endl;
+		}
+	}
+	else {
+		cout << i << " = " ;
+		while (index > 0)
+		{
+			sortV.clear();
+			for (int j = 0; j < d.size(); j++) {
+				if (i - d[j] >= 0) {
+					sortV.push_back(d[j]);
+				}
+
+			}
+			sort(sortV.begin(), sortV.end());
+			int val = sortV[sortV.size() - 1];
+			index -= c[val];
+			i -= val;
+			if (index > 0) {
+				cout << val << " +";
+			}
+			else {
+				cout << val ;
+			}
+		}
+	}
+
+
+	//while (index > 0)
+	//{
+	//	sortV.clear();
+	//	for (int j = 0; j < d.size(); j++) {
+	//		if (index - d[j] >= 0) {
+	//			sortV.push_back(index - d[j]);
+	//		}
+
+	//	}
+	//	sort(sortV.begin(), sortV.end());
+	//	
+	//	int val = sortV[sortV.size() - 1];
+	//	index-= val;
+	//	if (index > 0) {
+	//		cout << val << " +";
+	//	}
+	//	else {
+	//		cout << val ;
+	//	}
+	//	
+
+	//	//vector<int>::iterator fit = find(d.begin(), d.end(), index);
+	//	//if (fit != d.end()) {
+	//	//	cout << d[*fit] << "+";
+	//	//}
+	//	
+	//	
+	//}
+	
+
+}
+
